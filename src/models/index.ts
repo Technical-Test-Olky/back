e4d7f78bdd441cd4b2f01f9d1f0c606cfb1d2061
MDrapala <drapalamathieu95@gmail.com>
2024-09-04 13:29:34 +0200

    clean key_image

--- src/models/index.ts
+++ src/models/index.ts
@@ -12,12 +12,4 @@ Database.sequelize = connectSQLServer;
 // Images
 Database.images = ImageModel(connectSQLServer, Sequelize);
 
-// Key_images
-// Database.key_images = KeyImageModel(connectSQLServer, Sequelize);
-
-// Database.key_images.belongsTo(Database.images, {
-//   through: "key_image",
-//   foreignKey: "image_id",
-// });
-
 export = Database;
